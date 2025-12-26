22a70dc96fdcf77b763a8fc985d29df6dafbd63a
Sulthan Ananda Putra <sulthananandaputra79@gmail.com>
2025-12-26 15:24:36 +0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+function openModal(id) {
+  document.getElementById(id).style.display = "flex";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Tutup modal jika klik di luar konten
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
